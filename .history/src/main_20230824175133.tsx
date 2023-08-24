b4073a5b1d089ae6d1e052fca249d11f60c0eebf
BarbaraCD <barbaracastrod7@gmail.com>
2023-08-24 17:52:01 -0300

    commit 24/08

--- .history/src/main_20230824175133.tsx
+++ .history/src/main_20230824175133.tsx
@@ -7,6 +7,7 @@ import { theme } from './styles/Theme.ts'
 import App from './App.tsx'
 import { Routes } from './routes/index.tsx'
 import { Header } from './components/Header.tsx'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
